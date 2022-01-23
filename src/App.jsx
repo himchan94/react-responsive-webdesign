d2a79ea067363eb09f17b54905af799720e45f61
himchan94 <himchan9494@gmail.com>
2022-01-23 14:35:11 +0900

    container setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./App.css";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  WhatGPT3,
+  Header,
+  Features,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
-  return <div>GPT-3</div>;
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
